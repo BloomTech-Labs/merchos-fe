c7b0446de051e171b7f13719cbee97521dd6aa1e
Christopher Ksiazak <cksiazak@gmail.com>
2020-02-19 22:24:04 -0700

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-require('dotenv').config();
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
 
 module.exports = {
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    config.node = { fs: 'empty' };
+    config.plugins = config.plugins || [];
+
+    config.plugins = [...config.plugins];
+
+    return config;
+  },
   env: {
     BACKEND_URL: process.env.BACKEND_URL
   }
