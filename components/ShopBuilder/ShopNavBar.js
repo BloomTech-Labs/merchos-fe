915bb5dc970c82f118234966e8ed7a542d35bdb3
Christopher Ksiazak <cksiazak@gmail.com>
2020-03-16 20:20:33 -0600

    added basic layout for navbar

--- components/ShopBuilder/ShopNavBar.js
+++ components/ShopBuilder/ShopNavBar.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
-import {axiosWithAuth} from './../../utils/axiosWithAuth';
+import { axiosWithAuth } from './../../utils/axiosWithAuth';
+
+const NavBar = styled.section`
+  display: flex;
+  width: 100%;
+`;
+
+const InnerNavBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const UserInput = () => {
+  return <button>Button</button>;
+};
 
 const ShopNavBar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <NavBar>
+      <InnerNavBar>
+        <UserInput />
+        <div>
+          <UserInput />
+          <UserInput />
+          <UserInput />
+        </div>
+        <UserInput />
+      </InnerNavBar>
+    </NavBar>
+  );
+};
 
-export default ShopNavBar
+export default ShopNavBar;
--- pages/shopbuilder.js
+++ pages/shopbuilder.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
-import { Responsive, WidthProvider } from "react-grid-layout";
-import { connect } from "react-redux";
+import React, { useState, Fragment } from 'react';
+import { Responsive, WidthProvider } from 'react-grid-layout';
+import { connect } from 'react-redux';
+import styled, { keyframes } from 'styled-components';
+
+// Components
 import {
   updateLayoutAction,
   onDrop
-} from "../store/actions/ShopBuilderActions";
-import ModalLayout from "../components/ShopBuilder/ModalLayout";
-import SideBar from "../components/ShopBuilder/SideBar";
-import styled , {keyframes} from "styled-components";
+} from '../store/actions/ShopBuilderActions';
+import ModalLayout from '../components/ShopBuilder/ModalLayout';
+import SideBar from '../components/ShopBuilder/SideBar';
+import ShopBuilderNav from '../components/ShopBuilder/ShopNavBar';
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
@@ -30,20 +33,20 @@ const SideBarContainer = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 45px 45px 45px 45px;
-  background: #EEE;
-  box-shadow: -2px -2px 6px 2px #FFF, 2px 2px 6px 2px #8e9093;
+  background: #eee;
+  box-shadow: -2px -2px 6px 2px #fff, 2px 2px 6px 2px #8e9093;
   position: fixed;
   z-index: 900;
-  `;
+`;
 
-const SideBarTitle= styled.div`
-background: #464646;
-color: white;
-font-size: 1.4vw;
-border-radius: 45px 45px 0  0;
-padding: 13%;
-text-align: center;
-height: 60px;
+const SideBarTitle = styled.div`
+  background: #464646;
+  color: white;
+  font-size: 1.4vw;
+  border-radius: 45px 45px 0 0;
+  padding: 13%;
+  text-align: center;
+  height: 60px;
 `;
 
 const DropZone = styled.div`
@@ -55,17 +58,17 @@ const DropZone = styled.div`
 `;
 
 const ShopContainer = styled.div`
-  ${props => (props.blurContainer ? "filter: blur(2px);" : "")}
+  ${props => (props.blurContainer ? 'filter: blur(2px);' : '')}
 `;
 
 const ClosedSideBarButton = styled.div`
-  background: #AAACB1;
+  background: #aaacb1;
   border-radius: 75px;
   font-size: 15px;
   display: flex;
   font-weight: 700;
   border-radius: 0 45px 45px 0;
-  box-shadow: -2px -2px 6px 2px #FFF, 2px 2px 6px 2px #8e9093;
+  box-shadow: -2px -2px 6px 2px #fff, 2px 2px 6px 2px #8e9093;
   padding: 10vw 1vw 1vw 1vw;
 `;
 
@@ -81,23 +84,24 @@ const ShopBuilder = props => {
   };
 
   // function to open and close sidebar
-  const [SideBarDisplay, setSideBarDisplay] = useState(true)
-  function openClose(){
-    if(SideBarDisplay){
+  const [SideBarDisplay, setSideBarDisplay] = useState(true);
+  function openClose() {
+    if (SideBarDisplay) {
       setSideBarDisplay(false);
-      document.getElementById("dropZone").style.marginLeft = "3vw"
-    } else{ 
+      document.getElementById('dropZone').style.marginLeft = '3vw';
+    } else {
       setSideBarDisplay(true);
-      document.getElementById("dropZone").style.marginLeft = "9vw"}
+      document.getElementById('dropZone').style.marginLeft = '9vw';
+    }
   }
-  
+
   const placeholderSize = id => {
     switch (id) {
-      case "banner":
+      case 'banner':
         return { w: 12, h: 2 };
-      case "product-container":
+      case 'product-container':
         return { w: 1, h: 2 };
-      case "store-name":
+      case 'store-name':
         return { w: 12, h: 1 };
       default:
         return { w: 1, h: 1 };
@@ -105,32 +109,54 @@ const ShopBuilder = props => {
   };
 
   return (
-    <div>
+    <Fragment>
+      <ShopBuilderNav />
       <ModalLayout displayModal={displayModal} display={display} />
       <ShopContainer blurContainer={displayModal}>
         <Button onClick={display}>Show Layouts</Button>
         <Page>
           {/* side bar that you drag stuff from */}
           {/* side bar can be toggled open and close */}
-          {SideBarDisplay ? <SideBarContainer>
-            <SideBarTitle>Draggable<br/>Items</SideBarTitle>
-            {sidebarLayout.map((item, index) => {
-              return (
-                <SideBar
-                  key={index}
-                  content={item.content}
-                  itemId={item.id}
-                  setDragId={setDragId}
-                />
-              );
-            })}
-            <SideBarTitle onClick={() => openClose()} style={{height: '5vh', borderRadius:"0 0 45px 45px", fontSize: "1vw", cursor: "pointer" }} >close</SideBarTitle>
-          </SideBarContainer>
-          : <ClosedSideBarButton onClick={() => openClose()}>O<br/>P<br/>E<br/>N<br/>&nbsp;<br/>D<br/>R<br/>A<br/>G<br/>A<br/>B<br/>E<br/>L<br/>S</ClosedSideBarButton>}
+          {SideBarDisplay ? (
+            <SideBarContainer>
+              <SideBarTitle>
+                Draggable
+                <br />
+                Items
+              </SideBarTitle>
+              {sidebarLayout.map((item, index) => {
+                return (
+                  <SideBar
+                    key={index}
+                    content={item.content}
+                    itemId={item.id}
+                    setDragId={setDragId}
+                  />
+                );
+              })}
+              <SideBarTitle
+                onClick={() => openClose()}
+                style={{
+                  height: '5vh',
+                  borderRadius: '0 0 45px 45px',
+                  fontSize: '1vw',
+                  cursor: 'pointer'
+                }}
+              >
+                close
+              </SideBarTitle>
+            </SideBarContainer>
+          ) : (
+            <ClosedSideBarButton onClick={() => openClose()}>
+              O<br />P<br />E<br />N<br />
+              &nbsp;
+              <br />D<br />R<br />A<br />G<br />A<br />B<br />E<br />L<br />S
+            </ClosedSideBarButton>
+          )}
           {/* area where you assemble the shop builder */}
-          <DropZone id="dropZone">
+          <DropZone id='dropZone'>
             <ResponsiveGridLayout
-              className="layout"
+              className='layout'
               layouts={{ lg: currentLayout }}
               breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 360 }}
               cols={{ lg: 12, md: 9, sm: 6, xs: 3 }}
@@ -148,14 +174,14 @@ const ShopBuilder = props => {
                 i: `${dragId}__dropping-elem__`,
                 ...placeholderSize(dragId)
               }}
-              style={{ background: "blue", minHeight: "500px" }}
+              style={{ background: 'blue', minHeight: '500px' }}
             >
               {currentLayout.map((gridItem, index) => {
                 return (
                   <GridItemContainer key={gridItem.i}>
                     {props.state.Page.content.length
                       ? props.state.Page.content[index].content
-                      : "+"}
+                      : '+'}
                   </GridItemContainer>
                 );
               })}
@@ -163,7 +189,7 @@ const ShopBuilder = props => {
           </DropZone>
         </Page>
       </ShopContainer>
-    </div>
+    </Fragment>
   );
 };
 
