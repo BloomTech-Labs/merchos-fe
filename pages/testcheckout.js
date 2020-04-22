c02c1e1a03d09ec89bcf36a6739098580e022034
AlbertoGalvan001 <54903554+AlbertoGalvan001@users.noreply.github.com>
2020-04-21 23:43:51 -0400

    title, item column and link created

--- pages/testcheckout.js
+++ pages/testcheckout.js
@@ -1,7 +1,61 @@
 import React from "react";
+import styled from "styled-components";
+import Router from "next/router";
+import { IoIosArrowBack } from "react-icons/io";
+
+const Head = styled.div`
+  display: flex;
+  width: 45%;
+  align-content: center;
+  align-items: center;
+  justify-content: space-between;
+  margin-left: 2%;
+`;
+
+const Link = styled.h3`
+  font-size: 2rem;
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: flex-start;
+  color: #0751ff;
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
+  font-weight: 800;
+  display: flex;
+  justify-content: center;
+  padding-top: 2%;
+  color: #000000;
+`;
+
+const Items = styled.h3`
+  font-size: 3rem;
+  font-weight: 800;
+  display: flex;
+  justify-content: start;
+  margin-left: 2%;
+  color: #000000;
+`;
+
+const ItemCard = styled.div`
+  height: ;
+`;
 
 const TestCheckout = () => {
-  return <div>TESTING</div>;
+  return (
+    <div>
+      <Head>
+        <Link>
+          <IoIosArrowBack size="4rem" color="#0751ff" />
+          Back To Shop
+        </Link>
+        <Title>Checkout</Title>
+      </Head>
+      <Items>Items:</Items>
+    </div>
+  );
 };
 
 export default TestCheckout;
